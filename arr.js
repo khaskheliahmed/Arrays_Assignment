a7bea10c93592e847e7a394e8d444ae18b132a71
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2024-08-03 01:10:05 +0500

    arrayproblem

---
+++ arr.js
@@ -0,0 +1,176 @@
+
+//1
+
+
+var student = ['','','',''];
+console.log(student)
+
+
+
+//2
+
+var studentName = ['ali','ahmed','khalid']
+console.log(studentName)
+
+//3
+
+var fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits)
+
+//4
+
+var number = ['1','22','33','44','55.5'];
+console.log(number);
+
+//5
+
+
+var boolean = ['ture','false']
+console.log(boolean)
+
+//6
+
+var mixedArr =['ali','ahmed','43','ture','false',['1','2','3']]
+console.log(mixedArr)
+
+//7
+
+
+var  mobileNetworksInPakistan = ["Jazz", "Telenor", "Zong", "Ufone", "Warid"];
+console.log(mobileNetworksInPakistan)
+
+//8
+
+
+const qualifications = ["SSC", "HSC", "BCS","BS","BCOM", "MS", "M. Phil.","PhD"]
+   console.log(qualifications);
+
+
+//9
+
+var topMovies2015 = []
+
+topMovies2015.push('Bajrangi Bhaijaan')
+topMovies2015.push('Gabbar Is Back')
+topMovies2015.push('Dilwale')
+topMovies2015.push('ABCD 2')
+topMovies2015.push('Prem Ratan Dhan Payo')
+topMovies2015.push('Baahubali: The Beginning')
+
+console.log(topMovies2015)
+
+var moviesArr = "Number of movies in the array"   +  topMovies2015.length ;
+
+console.log(moviesArr)
+
+
+//10
+
+var  favoriteCars = ["Tesla", "BMW", "Audi R8", "Mercedes-Benz S-Class"]
+
+console.log(favoriteCars)
+// console.log(favoriteCars[0])
+var firstIndex = 0;
+console.log(firstIndex)
+
+var carfirtsindex = favoriteCars[firstIndex]
+console.log(carfirtsindex);
+
+
+var lastIndex = favoriteCars.length -1
+console.log(lastIndex);
+
+console.log(favoriteCars[3]);
+
+
+
+//11
+// ....
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+//.
+// .
+// .
+
+
+
+
+
+
+//12
+
+
+
+var  colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+
+console.log(colors)
+
+colors.push('white')
+colors.push('gray')
+colors.pop('')
+console.log(colors)
+
+colors.unshift('black')
+colors.unshift('lightblue')
+colors.shift('')
+
+console.log(colors)
+
+//13
+
+var scoresOfstudent = ['320','230','480','120']
+
+var Arr= scoresOfstudent.sort()
+
+console.log(Arr)
+
+
+//14
+
+var fruits = ['strawberry', 'apple', 'orange', 'banana'];
+
+var sortArray = fruits.sort()
+console.log(sortArray)
+
+//15
+
+var city = ['karachi','hyderabad','Isalamabad','lahore','larkana']
+
+var cityNames = city.splice(2,2)
+console.log(cityNames)
+
+
+//16
+
+
+var arr = ['This', 'is', 'my',   'cat'];
+
+var joinArray = arr.join("")
+console.log(joinArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
